d56f93abe765fe827a7d8fa406c3b51269a19c2d
Thiruppathiraj93 <senthithiru0@gmail.com>
2025-10-31 23:21:11 +0530

    Add files via upload

---
+++ javascript/loops/demo.js
@@ -0,0 +1,27 @@
+//task 1
+let a=10
+if(a>=0){
+    document.writeln("possitive")
+}else{
+    document.writeln("nagative")
+}
+document.writeln("<br/>")
+document.writeln("task2 <br>")
+let b=20
+if(b%2==0){
+    document.writeln(" divisible by 2 ,so even")
+}else{
+    document.writeln("add")
+}
+document.writeln("<br/>")
+document.writeln("task 3 <br>")
+let age=22
+if(age>=18){
+    document.writeln("u r eligible")
+}else{
+    document.writeln("u r not eligible")
+}
+document.writeln("<br/>")
+document.writeln("task 4 <br>")
+let vari=78
+if()
