a3577217db1cb9773955539ead68102f0c2072bb
Thiruppathiraj93 <senthithiru0@gmail.com>
2025-10-31 23:08:34 +0530

    Add files via upload

---
+++ javascript/operator/script.js
@@ -0,0 +1,161 @@
+//arithmatic operator
+//task 1
+const a = 200
+const b= 100
+ sum=a+b
+diff=a-b
+product=a*b
+quotient=a/b
+document.writeln(" task 1<br/>")
+document.writeln("sum is :"+sum)
+document.writeln("different is :"+diff)
+document.writeln("product is :"+product)
+document.writeln("quotient is :"+quotient)
+document.writeln("<br/>")
+
+//task 2
+
+document.writeln(" task 2<br/>")
+const first=76
+const second=90
+const three=86
+ total= first+second+three
+avg=total/3
+document.writeln("avg is :"+avg)
+document.writeln("<br/>")
+
+//task 3
+
+document.writeln(" task 3<br/>")
+const length=5
+const breadth=5
+rectangle=length*breadth
+document.writeln("rectangle is"+rectangle)
+document.writeln("<br/>")
+
+//task 4
+document.writeln(" task 4<br/>")
+let cel=32
+let fahrenheitTemp = celsiusToFahrenheit(cel);
+function celsiusToFahrenheit(celsius) {
+  value= (celsius * 9/5) + 32;
+}
+document.writeln("celcious to fahren is:",+value)
+document.writeln("<br/>")
+
+/*task 5
+document.writeln(" task 5<br/>")
+//const devident=prompt("enter a devident:")
+document.writeln("devidentis ;",devident )
+//const divisor=prompt("enter a  divisor")
+document.writeln("divisor is:",divisor)
+/*let remainder = dividend % divisor;
+document.writeln("remainder is:",remainder)*/
+document.writeln("<br/>")/
+//assignment operator
+//task 6
+document.writeln(" task 6<br/>")
+var d=5
+document.writeln(d+=10)
+document.writeln("<br/>")
+
+//task7
+document.writeln(" task 7<br/>")
+var x=10
+document.writeln(x*=5)
+document.writeln("<br/>")
+
+//task8
+document.writeln(" task 8<br/>")
+var divide=10
+document.writeln(divide/=2)
+document.writeln("<br/>")
+//task9
+document.writeln(" task 9<br/>")
+let s=10
+document.writeln(s-=3)
+document.writeln("<br/>")
+//task 10
+document.writeln(" task 10<br/>")
+let n=10
+let o=5
+document.writeln("addition:",n+=o)
+document.writeln("sub:",n-=o)
+document.writeln("mul:",n*=o)
+document.writeln("div:",n/=o)
+document.writeln("<br/>")
+
+//comparison operator
+//task 11
+document.writeln(" task 11<br/>")
+{var e=10
+var f=11
+if (e==f)
+{
+ document.writeln("equal") 
+}else{
+  document.writeln("not eqaual")
+}
+document.writeln("<br/>")
+//task 12
+document.writeln(" task 12<br/>")
+if(e>f) 
+{
+document.writeln("true")
+}else{
+  document.writeln("false")
+}
+document.writeln("<br/>")
+//task 13
+document.writeln(" task 13<br/>")
+var name1="thiru"
+var name2="aruna"
+if(name1==name2)
+{
+  document.writeln("same")
+}else{
+  document.writeln("notsame")
+}
+document.writeln("<br/>")
+//task 14
+document.writeln(" task 14<br/>")
+if (2<=100){
+  document.writeln("yes")
+}else{
+  document.writeln("no")
+}
+document.writeln("<br/>")
+//task 15
+document.writeln(" task 15<br/>")
+if(e===f){
+  document.writeln("strictly equal")
+}else{
+  document.writeln("strictly not equal")
+  document.writeln("<br/>")
+}}
+//logical operator
+//task 16
+document.writeln(" task 16<br/>")
+if(e>=10 && e<=50){
+  document.writeln("available")
+}else{
+  document.writeln("not available")
+}
+document.writeln("<br/>")
+//task 17
+document.writeln(" task 17<br/>")
+if(e<=10 || e>=50){
+  document.writeln("available")
+}else{
+  document.writeln("not available")
+}
+document.writeln("<br/>")
+//task 18
+document.writeln(" task 18<br/>")
+
+
+
+
+
+
+
