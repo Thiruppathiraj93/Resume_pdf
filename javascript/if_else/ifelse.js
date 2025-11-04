c0bd5e76fc1ccbb5b1e86e5bfad529fc1d1ee1f0
Thiruppathiraj93 <senthithiru0@gmail.com>
2025-11-04 11:03:18 +0530

    Add files via upload

---
+++ javascript/if_else/ifelse.js
@@ -0,0 +1,70 @@
+//task 1
+let a=10
+if(a>=0){
+    document.writeln("possitive")
+}else{
+    document.writeln("nagative")
+}
+document.writeln("<br/>")
+document.writeln("task2 <br>")
+let b=20
+if(b%2==0){
+    document.writeln(" divisible by 2 ,so even")
+}else{
+    document.writeln("add")
+}
+document.writeln("<br/>")
+document.writeln("task 3 <br>")
+let age=22
+if(age>=18){
+    document.writeln("u r eligible")
+}else{
+    document.writeln("u r not eligible")
+}
+document.writeln("<br/>")
+document.writeln("task 4 <br>")
+let value=true
+document.writeln(typeof(value))
+document.writeln("<br/>")
+document.writeln("task 5 <br>")
+let x=343
+let y=4545
+if(a>b){
+    document.writeln("x is greater")
+}
+else{
+    document.writeln("y is greater")
+}
+document.writeln("<br/>")
+document.writeln("task 6 <br>")
+if(x==y){
+    document.writeln("equal")
+}else{
+    document.writeln("not equal")
+}
+document.writeln("<br/>")
+document.writeln("task 8 <br>")
+let given_value=10
+if(given_value===0){
+    document.writeln("zero")
+}else{
+    document.writeln("not zero")
+}
+document.writeln("<br/>")
+document.writeln("task 9 <br>")
+let eng=57
+if(eng>=35){
+    document.writeln("pass")
+}else{
+    document.writeln("fail")
+}
+document.writeln("<br/>")
+document.writeln("task 10 <br>")
+let temp=34
+if(temp<20){
+    document.writeln("cold")
+}else if(20<30){
+    document.writeln("Warm")
+}else{
+    document.writeln("Hot")
+}
