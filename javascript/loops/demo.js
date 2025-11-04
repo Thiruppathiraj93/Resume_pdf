ca8a36ec8cb3939fb8e7c88ad2dfbde2ec68cc31
Thiruppathiraj93 <senthithiru0@gmail.com>
2025-11-04 10:58:19 +0530

    Update loop.js

--- javascript/loops/demo.js
+++ javascript/loops/demo.js
@@ -1,27 +1,51 @@
-//task 1
-let a=10
-if(a>=0){
-    document.writeln("possitive")
-}else{
-    document.writeln("nagative")
+document.writeln("<br> task 1 <br/>")
+
+for(let i=1;i<=10;i++){
+    document.writeln(i)
 }
-document.writeln("<br/>")
-document.writeln("task2 <br>")
-let b=20
-if(b%2==0){
-    document.writeln(" divisible by 2 ,so even")
-}else{
-    document.writeln("add")
+document.writeln("<br>")
+document.writeln("task 2 <br>")
+let i
+for(i=0;i<=20;i += 2){
+    document.writeln(i)
 }
-document.writeln("<br/>")
+document.writeln("<br>")
 document.writeln("task 3 <br>")
-let age=22
-if(age>=18){
-    document.writeln("u r eligible")
-}else{
-    document.writeln("u r not eligible")
+let sum=0;
+for (let i=1;i<=100;i++){
+    sum += i
 }
-document.writeln("<br/>")
+document.writeln( "Sum of digit 1 to 100 is:",sum)
+document.writeln("<br>")
 document.writeln("task 4 <br>")
-let vari=78
-if()
+for( let b=1 ;b<=10 ;b++){
+     let c =document.writeln(b,"*5=",b*5)
+     document.writeln("<br>")
+}
+document.writeln("<br>")
+document.writeln("task 5 <br>")
+ var num=[22,33,44,55,66]
+ for(let x=0;x<=num.length;x++){
+    document.writeln(num[x])
+    document.writeln("<br>")
+ }
+document.writeln("<br>")
+document.writeln("task 6 <br>")
+var num=5
+fact=1
+for(let i=1;i<=num;i++){
+    fact=fact*i
+} 
+document.writeln("factorial og given num is :",fact)
+document.writeln("<br>")
+document.writeln("task 7 <br>")
+
+for(let p=1;p<=50;p+2){
+
+    document.writeln(p)
+}
+
+
+
+
+
