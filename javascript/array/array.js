2ae53620daab97b20419dec263b70146360b1274
Thiruppathiraj93 <senthithiru0@gmail.com>
2025-11-07 10:29:14 +0530

    Add files via upload

---
+++ javascript/array/array.js
@@ -0,0 +1,68 @@
+document.writeln("task 1")
+document.writeln("<br/>")
+let fruits=["apple", "orange","banana","grapes","mango"]
+fruits.push("guava")
+document.writeln(fruits)
+document.writeln("<br>")
+document.writeln("task 2")
+document.writeln("<br/>")
+fruits.pop()
+document.writeln(fruits)
+document.writeln("<br>")
+document.writeln("task 3")
+document.writeln("<br/>")
+fruits.unshift("guava")
+document.writeln(fruits)
+document.writeln("<br>")
+document.writeln("task 4")
+document.writeln("<br/>")
+fruits.shift()
+document.writeln(fruits)
+document.writeln("<br>")
+document.writeln("task 5")
+document.writeln("<br/>")
+document.writeln(fruits.length)
+document.writeln("<br>")
+document.writeln("task 6")
+document.writeln("<br/>")
+document.writeln(fruits.join(", "))
+document.writeln("<br>")
+document.writeln("task 7")
+document.writeln("<br/>")
+let colors = ["red", "blue", "green", "yellow", "purple"];
+let lastColor = colors[colors.length - 1];
+document.writeln("The last color is:", lastColor);
+document.writeln("<br>")
+document.writeln("task 8")
+document.writeln("<br/>")
+let names=["boopathi","silambu","dinesh"]
+names.push("thiru","aruna")
+document.writeln(names)
+document.writeln("<br>")
+names.shift()
+document.writeln(names)
+document.writeln("<br>")
+document.writeln("task 9")
+document.writeln("<br/>")
+let fruitss = ["apple", "banana", "orange"];
+let sentence = "I like " + fruitss.join(", ");
+
+document.writeln(sentence);
+document.writeln("<br>")
+document.writeln("task 10")
+document.writeln("<br/>")
+let queue = [];
+queue.push("dineash");
+queue.push("thiru");
+queue.push("aruna");
+document.writeln("After adding:", queue);
+queue.pop();
+document.writeln("After removing:", queue);
+
+
+
+
+
+
+
+
